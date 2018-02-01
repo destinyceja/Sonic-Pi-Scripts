808c259dc00b47c584bc33eb9da1e23e1ec90830
destinyceja <33135802+destinyceja@users.noreply.github.com>
2018-02-01 14:47:58 -0800

    Add files via upload

---
+++ whitestripes.rb
@@ -0,0 +1,21 @@
+use_bpm 124
+#first notes
+use_synth:tri
+4. times do
+  play:e3
+  sleep 0.7
+  play:e3
+  sleep 0.7
+  play:g3
+  sleep 1
+  play:e3
+  sleep 0.7
+  #second measure
+  play:d3
+  sleep 0.7
+  play:c3
+  sleep 0.7
+  play:b2
+  sleep 1
+
+end
