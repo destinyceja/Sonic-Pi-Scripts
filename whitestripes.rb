7043018582debf1e076606133209acf6aed61263
destinyceja <33135802+destinyceja@users.noreply.github.com>
2018-02-12 14:09:07 -0800

    Update whitestripes.rb

--- whitestripes.rb
+++ whitestripes.rb
@@ -1,21 +1,25 @@
-use_bpm 124
+# white stripes seven nation army
+#sheet music- eric hand outs
+#bpm 120
+use_bpm 120
 #first notes
-use_synth:tri
-4. times do
-  play:e3
-  sleep 0.7
-  play:e3
-  sleep 0.7
-  play:g3
-  sleep 1
-  play:e3
-  sleep 0.7
-  #second measure
-  play:d3
-  sleep 0.7
-  play:c3
-  sleep 0.7
-  play:b2
-  sleep 1
-
+use_synth:saw
+with_fx :flanger do
+  4. times do
+    play:e3
+    sleep 0.8
+    play:e3
+    sleep 0.7
+    play:g3
+    sleep 0.7
+    play:e3
+    sleep 0.7
+    #second measure
+    play:d3
+    sleep 0.7
+    play:c3
+    sleep 0.8
+    play:b2
+    sleep 1.5
+  end
 end
